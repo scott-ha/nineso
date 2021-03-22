9fdc4ef72cec8b9d3de6d60609ae095434b62f23
scott-ha <haneul9209@gmail.com>
2021-03-22 20:37:10 +0900

    HN-rcmd1,2,3

--- routes/rcmd.js
+++ routes/rcmd.js
@@ -1,51 +1,51 @@
 const request = require('request');
 const req_modules = require('./req_modules');
 
-var res_data;
+var res_data_1, res_data_2, res_data_3;
 
 // bed_rcmd
 exports.rcmd_1 = function() {
   request.get(req_modules.req_get('/api/rcmd/1'), function(error, response, body) {
-    res_data = JSON.parse(body);
-    res_data = res_data.data;
+    res_data_1 = JSON.parse(body);
+    res_data_1 = res_data_1.data;
 
     // price formatting
-    for (var i = 0; i < res_data.length; i++) {
-      res_data[i].sales_price = res_data[i].sales_price.format();
+    for (var i = 0; i < res_data_1.length; i++) {
+      res_data_1[i].sales_price = res_data_1[i].sales_price.format();
     }
 
   });
-  return res_data;
+  return res_data_1;
 }
 
 // living_rcmd
 exports.rcmd_2 = function() {
   request.get(req_modules.req_get('/api/rcmd/2'), function(error, response, body) {
-    res_data = JSON.parse(body);
-    res_data = res_data.data;
+    res_data_2 = JSON.parse(body);
+    res_data_2 = res_data_2.data;
 
     // price formatting
-    for (var i = 0; i < res_data.length; i++) {
-      res_data[i].sales_price = res_data[i].sales_price.format();
+    for (var i = 0; i < res_data_2.length; i++) {
+      res_data_2[i].sales_price = res_data_2[i].sales_price.format();
     }
 
   });
-  return res_data;
+  return res_data_2;
 }
 
 // storage_rcmd
 exports.rcmd_3 = function() {
   request.get(req_modules.req_get('/api/rcmd/3'), function(error, response, body) {
-    res_data = JSON.parse(body);
-    res_data = res_data.data;
+    res_data_3 = JSON.parse(body);
+    res_data_3 = res_data_3.data;
 
     // price formatting
-    for (var i = 0; i < res_data.length; i++) {
-      res_data[i].sales_price = res_data[i].sales_price.format();
+    for (var i = 0; i < res_data_3.length; i++) {
+      res_data_3[i].sales_price = res_data_3[i].sales_price.format();
     }
 
   });
-  return res_data;
+  return res_data_3;
 }
 
 
